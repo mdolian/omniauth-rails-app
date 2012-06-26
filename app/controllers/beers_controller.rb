06257179535dc1c779973b3be3ffa80ad4b576e3
Matt Dolian <matt.dolian@gmail.com>
2012-06-26 17:09:03 -0400

    added beers controller

---
+++ app/controllers/beers_controller.rb
@@ -0,0 +1,7 @@
+class BeersController < ApplicationController
+  
+  def all_beers_55
+    render
+  end
+    
+end
---
+++ config/initializers/brewerydb.rb
@@ -0,0 +1,3 @@
+BreweryDB.configure do |config|
+  config.api_key = '95c6a1eb99572da28ae79bf00e2824ca'
+end
